31924016b6a421d5efa2398e4d5e31e74ebc1568
Velne Wang <velnewang@gmail.com>
2020-01-25 19:52:12 +0800

    ADD: etcd, etcd-quay

---
+++ etcd/etcd-quay-v3.4.3.sh
@@ -0,0 +1,19 @@
+docker run \
+	--rm -it \
+	-p 2379:2379 \
+	-p 2380:2380 \
+	--name etcd-quay-v3.4.3 \
+	quay.io/coreos/etcd:v3.4.3 \
+	/usr/local/bin/etcd \
+	--name s1 \
+	--data-dir /etcd-data \
+	--listen-client-urls http://0.0.0.0:2379 \
+	--advertise-client-urls http://0.0.0.0:2379 \
+	--listen-peer-urls http://0.0.0.0:2380 \
+	--initial-advertise-peer-urls http://0.0.0.0:2380 \
+	--initial-cluster s1=http://0.0.0.0:2380 \
+	--initial-cluster-token tkn \
+	--initial-cluster-state new \
+	--log-level info \
+	--logger zap \
+	--log-outputs stderr
